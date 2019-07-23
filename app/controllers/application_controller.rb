d388732c96abd39c9fd89c94174033223ecb130d
Rayan Castro <rayancdc@gmail.com>
2019-07-23 15:45:05 -0300

    Add navbar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :require_admin!
+
+  private
+
+  def require_admin!
+    unless current_user && current_user.admin
+      flash[:notice] = "Permissão negada. Favor logar como administrador."
+      redirect_to(root_path)
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :require_admin!, only: [:home]
   def home
+  end
+
+  def admin_control_panel
   end
 end
