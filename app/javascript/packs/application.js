1f9c8a9e813b04e32e41c1757d4d5c62b212a880
Mauricio Homma <mhhomma@gmail.com>
2019-07-24 16:07:24 -0300

    Add main banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,9 @@ $(".carousel").slick({
   infinite: false,
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true,
-  dots: true,
+  arrows: false,
+  prevArrow: "<div class='left-arrow'></div>",
+  nextArrow: "<div class='right-arrow'></div>",
+  dots: true
 });
 
