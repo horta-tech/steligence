c9cae6f8f8bbd69dbef9481e9ae1f71f0400be6f
Mauricio Homma <mhhomma@gmail.com>
2019-07-24 17:49:16 -0300

    Add cornerstone carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,20 @@
 import "bootstrap";
 import "slick-carousel";
 
-$(".carousel").slick({
+$(".banner-carousel").slick({
   infinite: false,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
-  prevArrow: "<div class='left-arrow'></div>",
-  nextArrow: "<div class='right-arrow'></div>",
   dots: true
 });
 
+$(".cornerstone-carousel").slick({
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: "<div class='left-arrow'></div>",
+  nextArrow: "<div class='right-arrow'></div>",
+  dots: false
+});
