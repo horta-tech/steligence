6f3b120008e9a1ed6b20409397672f916f8b7db5
Mauricio Homma <mhhomma@gmail.com>
2019-08-06 16:37:18 -0300

    Change section 2 desktop arrows

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,8 +14,8 @@ $(".cornerstone-carousel").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: true,
-  prevArrow: "<div class='left-arrow'></div>",
-  nextArrow: "<div class='right-arrow'></div>",
+  prevArrow: "<div class='left-arrow' style='left: -30px;'></div>",
+  nextArrow: "<div class='right-arrow' style='right: -30px;'></div>",
   dots: false
 });
 
@@ -24,7 +24,7 @@ $(".differential-carousel").slick({
   slidesToShow: 2,
   slidesToScroll: 2,
   arrows: true,
-  prevArrow: "<div class='left-arrow' style='left: -30px;'></div>",
+  prevArrow: "<div class='left-arrow' style='left: -100px;'></div>",
   nextArrow: "<div class='right-arrow' style='right: -30px;'></div>",
   dots: false,
   responsive: [
