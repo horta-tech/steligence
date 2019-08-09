598df620df699bc55276c7a7635dca3baa92db2d
Rayan Castro <rayancdc@gmail.com>
2019-08-09 12:21:34 -0300

    Add pdf, fix ie11, fix navbar mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :allow_ip]
-  skip_before_action :require_admin!, only: [:home, :allow_ip]
+  skip_before_action :authenticate_user!, only: [:home, :allow_ip, :download_handbook]
+  skip_before_action :require_admin!, only: [:home, :allow_ip, :download_handbook]
   # skip_before_action :staging_mode, only: [:allow_ip]
   def home
     @lead = Lead.new
   end
 
   def admin_control_panel
+  end
+
+  def download_handbook
+    send_file(
+    "#{Rails.root}/app/assets/files/Handbook_2018_web.pdf",
+    filename: "Handbook_2018_web.pdf",
+    type: "application/pdf"
+    )
   end
 
   def allow_ip
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/admin', to: "pages#admin_control_panel"
   get '/allow-ip', to: "pages#allow_ip"
+  get 'download_handbook', to: "pages#download_handbook", as: "download_handbook"
   resources :leads, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
